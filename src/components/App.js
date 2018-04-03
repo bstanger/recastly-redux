d791809c4dbd64b7a2c28323a426951797fc1eda
Becca Stanger <beccastanger@gmail.com>
2018-04-03 12:08:17 -0700

    Convert App to functional stateless

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,20 @@ import changeVideoList from '../actions/videoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
 
-export default class App extends React.Component {
+let App = () => (
   //TODO: swap out the React components below for the container components
   //  you wrote in the 'containers' directory.
-  render() {
-    return (
-      <div>
-        <Nav />
-        <div className="row">
-          <div className="col-md-7">
-            <VideoPlayerContainer />
-          </div>
-          <div className="col-md-5">
-            <VideoListContainer />
-          </div>
-        </div>
+  <div>
+    <Nav />
+    <div className="row">
+      <div className="col-md-7">
+        <VideoPlayerContainer />
       </div>
-    );
-  }
-}
+      <div className="col-md-5">
+        <VideoListContainer />
+      </div>
+    </div>
+  </div>
+)
+
+export default App;
