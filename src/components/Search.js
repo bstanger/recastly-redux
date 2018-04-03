af1544f452884df01fca5f3c334c8fdfc060e8ed
Becca Stanger <beccastanger@gmail.com>
2018-04-02 19:07:57 -0700

    Get most tests passing, except thunk

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,14 @@
 import React from 'react';
  
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: ''
-    };
-  }
-
-  handleInputChange(e) {
-    this.props.handleSearchInputChange(e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  }
-
   render() {
     return (
       <div className="search-bar form-inline">
         <input
           className="form-control"
           type="text"
-          value={this.state.value}
-          onChange={this.handleInputChange.bind(this)}
+          value={this.props.value}
+          onChange={this.props.handleSearchInputChange.bind(this)}
         />
         <button className="btn hidden-sm-down">
           <span className="glyphicon glyphicon-search"></span>
